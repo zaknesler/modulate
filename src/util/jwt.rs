71e3080f44f25fde46ff662db3fc4279eb14a52d
Zak Nesler <zak@nesler.dev>
2023-11-18 20:08:06 -0500

    wip

--- src/util/jwt.rs
+++ src/util/jwt.rs
@@ -6,25 +6,25 @@ use sha2::Sha256;
 use std::collections::BTreeMap;
 
 const JWT_EXPIRATION_DAYS: i64 = 90;
+const JWT_CLAIM_USER: &str = "sub";
+const JWT_CLAIM_ISSUED_AT: &str = "iat";
+const JWT_CLAIM_EXPIRES_AT: &str = "exp";
 
-pub const JWT_CLAIM_USER: &str = "sub";
-pub const JWT_CLAIM_ISSUED_AT: &str = "iat";
-pub const JWT_CLAIM_EXPIRES_AT: &str = "exp";
-
-/// Create a JWT for the given UUID
+/// Create a JWT for the given user ID
 pub fn sign_jwt(secret: &str, user_id: String) -> crate::Result<String> {
     let key: Hmac<Sha256> = Hmac::new_from_slice(secret.as_bytes())?;
     let now: DateTime<Utc> = Utc::now();
 
-    let mut claims = BTreeMap::new();
-    claims.insert(JWT_CLAIM_USER, user_id);
-    claims.insert(JWT_CLAIM_ISSUED_AT, now.to_rfc3339());
-    claims.insert(
-        JWT_CLAIM_EXPIRES_AT,
-        (now + Duration::days(JWT_EXPIRATION_DAYS)).to_rfc3339(),
-    );
-
-    claims.sign_with_key(&key).map_err(|err| err.into())
+    BTreeMap::from([
+        (JWT_CLAIM_USER, user_id),
+        (JWT_CLAIM_ISSUED_AT, now.to_rfc3339()),
+        (
+            JWT_CLAIM_EXPIRES_AT,
+            (now + Duration::days(JWT_EXPIRATION_DAYS)).to_rfc3339(),
+        ),
+    ])
+    .sign_with_key(&key)
+    .map_err(|err| err.into())
 }
 
 /// Extract the claims from a valid JWT
@@ -33,7 +33,7 @@ pub fn extract_claims(secret: &str, jwt: &str) -> crate::Result<BTreeMap<String,
     jwt.verify_with_key(&key).map_err(|err| err.into())
 }
 
-/// Verify that a JWT is valid and extract the user's ID
+/// Verify that a JWT is valid and extract the user ID
 pub fn verify_jwt(secret: &str, jwt: &str) -> crate::Result<String> {
     let claims = extract_claims(secret, jwt)?;
 
@@ -47,7 +47,7 @@ pub fn verify_jwt(secret: &str, jwt: &str) -> crate::Result<String> {
         return Err(Error::JwtExpired);
     }
 
-    // Attempt to extract the user's ID
+    // Attempt to extract the user ID
     Ok(claims
         .get(JWT_CLAIM_USER)
         .ok_or_else(|| Error::InvalidJwt)?
