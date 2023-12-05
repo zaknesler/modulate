9b2110a4c2d15bb1d66b5abba319d74a1f0e8027
Zak Nesler <zak@nesler.dev>
2023-12-04 22:29:27 -0500

    wip

--- src/api/token.rs
+++ src/api/token.rs
@@ -1,8 +1,10 @@
+use std::collections::HashSet;
+
 pub struct Token {
     pub access_token: String,
     pub token_type: String,
     pub expires_in: chrono::Duration,
     pub expires_at: chrono::DateTime<chrono::Utc>,
     pub refresh_token: String,
-    pub scope: Vec<String>,
+    pub scope: HashSet<String>,
 }
