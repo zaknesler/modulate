243c9c20587cfc03ed5e2eb7092acf5c00d94f1c
Zak Nesler <zak@nesler.dev>
2023-12-08 08:37:05 -0500

    fix

--- src/api/id.rs
+++ src/api/id.rs
@@ -10,7 +10,7 @@ impl FromStr for PlaylistId {
     type Err = ClientError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Regex::new(&r"(?:https?://open\.spotify\.com/playlist/|spotify:playlist:)?([a-zA-Z0-9]+)")?
+        Regex::new(&r"^(?:https?://open\.spotify\.com/playlist/|spotify:playlist:)?([a-zA-Z0-9]+)")?
             .captures(s)
             .and_then(|captures| Some(Self(captures.get(1)?.as_str().to_string())))
             .ok_or_else(|| ClientError::InvalidId(s.to_owned()))
@@ -29,30 +29,19 @@ mod test {
 
     #[test]
     fn it_fails_for_bad_ids() {
-        assert!(matches!(PlaylistId::from_str("some bad id"), Err(_)));
+        let test = |id: &str| matches!(PlaylistId::from_str(id), Err(_));
+
+        test("some bad id");
     }
 
     #[test]
     fn it_parses_uris_and_urls() {
         let expected = PlaylistId("EX3J5Phq9j7KcpkZJskhRP".to_string());
+        let test = |id: &str| assert_eq!(PlaylistId::from_str(id).unwrap(), expected);
 
-        assert_eq!(
-            PlaylistId::from_str("spotify:playlist:EX3J5Phq9j7KcpkZJskhRP").unwrap(),
-            expected
-        );
-
-        assert_eq!(
-            PlaylistId::from_str("https://open.spotify.com/playlist/EX3J5Phq9j7KcpkZJskhRP")
-                .unwrap(),
-            expected
-        );
-
-        assert_eq!(
-            PlaylistId::from_str(
-                "https://open.spotify.com/playlist/EX3J5Phq9j7KcpkZJskhRP?some=other&query=params"
-            )
-            .unwrap(),
-            expected
-        );
+        test("EX3J5Phq9j7KcpkZJskhRP");
+        test("spotify:playlist:EX3J5Phq9j7KcpkZJskhRP");
+        test("https://open.spotify.com/playlist/EX3J5Phq9j7KcpkZJskhRP");
+        test("https://open.spotify.com/playlist/EX3J5Phq9j7KcpkZJskhRP?some=other&query=params");
     }
 }
